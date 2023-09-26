ee439e59ea469d98cf16782ec783c4101bc9a161
Pierre Hugo <2152709+avoid3d@users.noreply.github.com>
2023-09-26 18:11:11 +0200

    Add skeleton for exporting as python lib

--- potionforge/src/lib.rs
+++ potionforge/src/lib.rs
@@ -1,14 +1,67 @@
-use pyo3::prelude::*;
+use models::{IngredientKey, Process};
+use pyo3::{prelude::*, types::PyDict};
+use recommend::IngredientCounts;
 
 pub mod enumerate;
 pub mod models;
 pub mod recommend;
 pub mod simulate;
 
-/// Formats the sum of two numbers as string.
+impl From<&str> for IngredientKey {
+    fn from(ingredient: &str) -> IngredientKey {
+        match ingredient {
+            "catnip" => IngredientKey::Catnip,
+            "lupine" => IngredientKey::Lupine,
+            "mandrake" => IngredientKey::Mandrake,
+            "nightshade" => IngredientKey::Nightshade,
+            "sage" => IngredientKey::Sage,
+            "thyme" => IngredientKey::Thyme,
+            "wormwood" => IngredientKey::Wormwood,
+            "anise" => IngredientKey::Anise,
+            "deadmans" => IngredientKey::Deadmans,
+            "deathcap" => IngredientKey::Deathcap,
+            "elven" => IngredientKey::Elven,
+            "flyagaric" => IngredientKey::Flyagaric,
+            "pluteus" => IngredientKey::Pluteus,
+            "wizards" => IngredientKey::Wizards,
+            "asporeus" => IngredientKey::Asporeus,
+            "stinkhorn" => IngredientKey::Stinkhorn,
+            _ => unreachable!(),
+        }
+    }
+}
+
+/// Recommends potions that can be crafted with the provided configuration
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn my_recommend(available_ingredients: &PyDict, arcane_power: i64) {
+    let mut rust_available_ingredients = IngredientCounts::new();
+
+    for (key, value) in available_ingredients.iter() {
+        let key: String = key.extract().unwrap();
+        let rust_key: IngredientKey = key.as_str().into();
+        let value: i32 = value.extract().unwrap();
+        rust_available_ingredients.insert(rust_key, value);
+    }
+
+    let processes = vec![Process::Blanch, Process::Crush];
+
+    let utilisation = 5;
+
+    let possible_recipes = enumerate::get_all_recipes(
+        &rust_available_ingredients
+            .iter()
+            .map(|(key, _)| key)
+            .collect(),
+        &processes,
+        arcane_power,
+    );
+
+    let _recommendations = recommend::recommend(
+        possible_recipes,
+        &rust_available_ingredients,
+        utilisation,
+        "0".to_string(),
+    );
 }
 
 /// A Python module implemented in Rust. The name of this function must match
@@ -16,6 +69,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 /// import the module.
 #[pymodule]
 fn potionforge(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_wrapped(wrap_pyfunction!(my_recommend))?;
     Ok(())
 }
