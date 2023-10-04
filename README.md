c99c6cd85b75bdd5ecbbfd86c98fc7eeebd5d8e0
Pierre Hugo <2152709+avoid3d@users.noreply.github.com>
2023-10-04 11:08:58 +0200

    Add criterion microbenchmarks

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # potions
 Potion Tycoon Potion Analysis Framework
+
+## Microbenchmarks
+```
+cargo install cargo-criterion
+cargo bench
+```
--- potionforge/benches/simulate_bench.rs
+++ potionforge/benches/simulate_bench.rs
@@ -1,9 +1,25 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use potionforge::{
     models::{traits::GetByKey, IngredientKey, INGREDIENTS},
     recommend::{AlchemistAttributes, BrandingCounts, MarketConditions},
     simulate::SimulateConfig,
 };
+
+pub fn collect_parts(c: &mut Criterion) {
+    let combination = vec![
+        INGREDIENTS.get_by_key(&IngredientKey::Asporeus).clone(),
+        INGREDIENTS.get_by_key(&IngredientKey::Wizards).clone(),
+    ];
+
+    let ingredients = combination.as_slice();
+
+    c.bench_function("collect_parts", |b| {
+        b.iter(|| {
+            let parts = ::potionforge::simulate::collect_parts(ingredients);
+            black_box(parts);
+        });
+    });
+}
 
 pub fn simulate(c: &mut Criterion) {
     let combination = vec![
@@ -19,9 +35,12 @@ pub fn simulate(c: &mut Criterion) {
     let ingredients = combination.as_slice();
 
     c.bench_function("simulate", |b| {
-        b.iter(|| ::potionforge::simulate::simulate(ingredients, &config));
+        b.iter(|| {
+            let recipe = ::potionforge::simulate::simulate(ingredients, &config);
+            black_box(recipe);
+        });
     });
 }
 
-criterion_group!(benches, simulate);
+criterion_group!(benches, simulate, collect_parts);
 criterion_main!(benches);
